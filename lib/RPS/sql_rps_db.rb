7fa48e67668ad0828fda1b554d071f79973ae927
Ashley McKemie <ashley.mckemie@gmail.com>
2014-05-19 11:54:31 -0500

    Create tests for and implement create_user and build_user methods

--- lib/RPS/sql_rps_db.rb
+++ lib/RPS/sql_rps_db.rb
@@ -1,4 +1,26 @@
 class RPS::DB
+  def initialize
+    @db = SQLite3::Database.new "rps.db"
+    users = "create table if not exists users(id integer, name string, password string, PRIMARY KEY(id));"
+    games = "create table if not exists games(id integer, mid integer, p1_pick string, p2_pick string, win_id integer, PRIMARY KEY(id));"
+    matches = "create table if not exists matches(id integer, p1_id integer, p2_id integer, win_id integer, PRIMARY KEY(id));"
+    invites = "create table if not exists invites(id integer, inviter integer, invitee integer, PRIMARY KEY(id));"
+
+    tables = [users, games, matches, invites]
+    tables.each do |table|
+      @db.execute(table)
+    end
+  end
+  # User CRUD methods
+  def create_user(data)
+    @db.execute("insert into users(name, password) values('#{data[:name]}', '#{data[:password]}');")
+    data[:id] = @db.execute("select last_insert_rowid();").flatten.first
+    build_user(data)
+  end
+
+  def build_user(data)
+    RPS::Users.new(data[:id], data[:name], data[:password])
+  end
 end
 
 module RPS
--- spec/sql_rps_db_spec.rb
+++ spec/sql_rps_db_spec.rb
@@ -14,4 +14,25 @@ describe 'db' do
     db2 = RPS.db
     expect(db1).to be(db2)
   end
+
+  # testing users
+  describe 'users' do
+    let(:user1) {RPS.db.create_user(:name => "Ashley", :password => "1234")}
+
+    it "creates a user with unique username and password" do
+      user1
+      expect(user1.name).to eq("Ashley")
+      expect(user1.password).to eq("1234")
+      expect(user1.id).to be_a(Fixnum)
+    end
+  end
+
+  # describe 'games' do
+  # end
+
+  # describe 'matches' do
+  # end
+
+  # describe 'invites' do
+  # end
 end
