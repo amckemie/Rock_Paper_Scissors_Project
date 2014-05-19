326768faa33ae0a9317de23de9e0dc770ffbf1be
Katrina Theodosopoulos <katrinaelaine6@gmail.com>
2014-05-19 16:44:00 -0500

    Created sign in method and passed all three tests.

--- spec/users_cmd_spec.rb
+++ spec/users_cmd_spec.rb
@@ -19,12 +19,32 @@ describe 'users_cmd' do
     it "should check for unique name and return error if not unique" do
       @cd.sign_up("Ashley", "adf")
       result = @cd.sign_up("Ashley", "asdf")
-      expect(result[:success?]).to eq(true)
+      expect(result[:error]).to eq("That username is already taken.")
     end
 
     it "should return an error if the password doesn't include at least one character" do
       result = @cd.sign_up("Alex", "1234")
       expect(result[:error]).to eq("Your password does not include at least one character.")
+    end
+  end
+
+  describe 'sign_in' do
+    it 'should return an error if the username does not exist' do
+      @cd.sign_up("Katrina", "abc123")
+      result = @cd.sign_in("Alex", "sdfh")
+      expect(result[:error]).to eq("That username does not exist.")
+    end
+
+    it 'should return an error if the password does not match' do
+      @cd.sign_up("Katrina", "abc123")
+      result = @cd.sign_in("Katrina", "sdfh")
+      expect(result[:error]).to eq("That is the wrong password.")
+    end
+
+    it 'should return true if the username and password are correct' do
+      @cd.sign_up("Katrina", "abc123")
+      result = @cd.sign_in("Katrina", "abc123")
+      expect(result[:success?]).to eq(true)
     end
   end
 end
