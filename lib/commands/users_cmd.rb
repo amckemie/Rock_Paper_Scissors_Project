60284b26f122af1c82c31d8569623332efc5f9b9
Katrina Theodosopoulos <katrinaelaine6@gmail.com>
2014-05-19 16:20:56 -0500

    Passed 3 tests for sign up command.

--- lib/commands/users_cmd.rb
+++ lib/commands/users_cmd.rb
@@ -4,7 +4,7 @@ class RPS::UsersCmd
     user = RPS.db.get_user(name)
     if user.name != nil
       return {success?: false, error: "That username is already taken."}
-    elsif /[a-zA-Z]/.match(password) == false
+    elsif /[a-zA-Z]/.match(password) == nil
       return {success?: false, error: "Your password does not include at least one character."}
     else
       RPS.db.create_user(:name => name, :password => password)
--- spec/users_cmd_spec.rb
+++ spec/users_cmd_spec.rb
@@ -7,20 +7,22 @@ describe 'users_cmd' do
 
   before(:each) do
     @cd = RPS::UsersCmd.new
+    RPS.db.clear_table("users")
   end
 
   describe "sign up" do
-    it "should check for unique name and return error if not unique" do
-      result = @cd.sign_up("Ashley", "asdf")
-      expect(result[:error]).to eq("That username is already taken.")
-    end
-
-    xit "should allow a user to sign up if name is unique" do
+    it "should allow a user to sign up if name is unique" do
       result = @cd.sign_up("Katrina", "abc123")
       expect(result[:success?]).to eq(true)
     end
 
-    xit "should return an error if the password doesn't include at least one character" do
+    it "should check for unique name and return error if not unique" do
+      @cd.sign_up("Ashley", "adf")
+      result = @cd.sign_up("Ashley", "asdf")
+      expect(result[:error]).to eq("That username is already taken.")
+    end
+
+    it "should return an error if the password doesn't include at least one character" do
       result = @cd.sign_up("Alex", "1234")
       expect(result[:error]).to eq("Your password does not include at least one character.")
     end
